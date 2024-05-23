5c8602bf30dc9949bd19cab37e41487241b976af
Lauren <lvavila80@ucatolica.edu.co>
2024-05-23 12:09:31 -0500

    prueba funcionalidad

--- SingleResponsibility/src/Uno/MainTest/test.js
+++ SingleResponsibility/src/Uno/MainTest/test.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Profesor_1 = require("./ClassSR/Profesor");
 var Curso_1 = require("./ClassSR/Curso");
-
-
+// Crear una instancia de Curso
 var cursoMatematicas = new Curso_1.Curso("Matemáticas");
-
+// Crear una instancia de Profesor
 var profesorJuan = new Profesor_1.Profesor("Juan");
-
+// El profesor enseña el curso
 profesorJuan.enseñar(cursoMatematicas);
